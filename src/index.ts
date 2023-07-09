5268c3cb74253c145ec54ce24bff2d38c97e4131
NaeYeonKyeong <yeunk0206@naver.com>
2023-07-09 21:06:53 +0900

    feat: to-do 동적 item별 이벤트 위임 - check button - remove button

--- src/index.ts
+++ src/index.ts
@@ -13,31 +13,47 @@ const todoLists: ToDoItem[] = [];
 // 필터링 조건
 let filterOption: FilterOption = 'all';
 
-const isIncluded = (item: ToDoItem): boolean => todoLists.some((i) => i.id === item.id);
+const isIncluded = (itemId: number): boolean => todoLists.some((i) => i.id === itemId);
 const getIndexOfItem = (id: number) => todoLists.map(i => i.id).indexOf(id);
 
-// todo: list를 화면에 보여주는 렌더 함수
-const render = (list: ToDoItem[]) => {
-    // ul 안에 list 맵 돌면서 li 삽입
+
+// list를 화면에 보여주는 렌더 함수
+const render = () => {
     const items = document.querySelector('.items');
-    if (items) {
+    const total = document.querySelector('.total-numbers');
+    const removeAllButton = document.querySelector('.remove-all');
+    const filed = document.getElementById('new-item-content');
+
+    const list = filterTodoLists();
+
+    if (items && total && removeAllButton && filed) {
         items.innerHTML = '';
+        // list 목록을 돌면서 ul 안에 li 삽입
         list.map((item) => {
             items.innerHTML += `
-                <li class='item ${item.isCompleted ? 'completed' : 'uncompleted'}'>
+                <li id='${item.id}' class='item ${item.isCompleted ? 'completed' : 'uncompleted'}'>
                         <div class='left'>
-                            <div class='check-box'>
+                            <span id='btn-check' class='btn-check'>
                                 <i class='icon fa-regular fa-square${item.isCompleted ? '-check' : ''}'></i>
-                            </div>
+                            </span>
                             <p class='content'>${item.content}</p>
                         </div>
                         <div class='right'>
-                            <i class='icon fa-solid fa-trash'></i>
-                            <i class='icon fa-solid fa-grip-vertical'></i>
+                            <span class='btn-remove'></span>
+                            <span class='btn-drag'></span>
                         </div>
                     </li>
             `;
         });
+
+        total.innerHTML = `총 ${list.length}개`;
+
+        removeAllButton.innerHTML = `
+            <i class='icon fa-solid  fa-trash'></i>
+            <span>전체 삭제(${list.length})</span>
+        `;
+
+        filed.focus();
     }
 };
 
@@ -53,52 +69,40 @@ const addTodoItem = () => {
         };
         todoLists.push(newItem);
         content.value = '';
-
-        // todo: render
-        render(todoLists);
     } else {
         alert('내용을 입력해 주세요');
     }
 };
 
-const removeItem = (item: ToDoItem) => {
-    if (isIncluded(item)) {
-        const indexToRemove = getIndexOfItem(item.id);
+const removeItem = (itemId: number) => {
+    if (isIncluded(itemId)) {
+        const indexToRemove = getIndexOfItem(itemId);
         // 삭제
         todoLists.splice(indexToRemove, 1);
-
-        // todo: render
-        render(todoLists);
     }
 };
 
 const removeAllItems = () => {
-    // 현재 조회한 목록을 돌면서 removeItem 실행하기
+    // 현재 필터링된 목록을 돌면서 removeItem 함수 실행하기
     const filtered = filterTodoLists();
     filtered.forEach((itemToRemove) => {
-        removeItem(itemToRemove);
+        removeItem(itemToRemove.id);
     });
-    render(todoLists);
 };
 
 const insertItem = (item: ToDoItem, itemToNext: ToDoItem) => {
     const nextIndex = getIndexOfItem(itemToNext.id);
 
     // 삭제
-    removeItem(item);
+    removeItem(item.id);
     // 원하는 위치에 item 다시 추가
     todoLists.splice(nextIndex, 0, item);
-
-    //todo: render
-    render(todoLists);
 };
 
 // 목록 필터링
 const filterTodoLists = (): ToDoItem[] => {
     let filtered: ToDoItem[] = [...todoLists];
     // 지금 체크된 상태값 확인후 해당 상태값인 아이템들만 필터링하기
-    // 전체 선택일때는 필터링을 하지 않도록 설계
-
     if (filterOption === 'completed' as FilterOption) {
         filtered = todoLists.filter((item) => item.isCompleted);
     } else if (filterOption == 'uncompleted' as FilterOption) {
@@ -116,17 +120,18 @@ const toggleTag = (clickedEl: HTMLElement) => {
     // 클릭한 요소에만 selected className 추가
     clickedEl.classList.toggle('selected');
     filterOption = clickedEl.getAttribute('data-tag-id') as FilterOption;
-
-    // 토글하는 순간 필터링한 결과 렌더
-    const filtered = filterTodoLists();
-    render(filtered);
 };
 
 window.onload = () => {
-    render(todoLists);
+    // 초기 렌더링
+    render();
+
     // 할일 추가 버튼
     const addButton = document.getElementById('btn-add');
-    addButton?.addEventListener('click', addTodoItem);
+    addButton?.addEventListener('click', () => {
+        addTodoItem();
+        render();
+    });
 
     // 전체 삭제 버튼
     const removeAllButton = document.getElementById('btn-remove-all');
@@ -134,25 +139,76 @@ window.onload = () => {
         const confirmToRemoveAll = confirm('모두 삭제 하시겠습니까?');
         if (confirmToRemoveAll) {
             removeAllItems();
+            render();
         }
     });
 
-    document.addEventListener('click', (event) => {
-        const target = event.target as HTMLElement;
-        const classList = target.classList;
+    // 태그 토글 이벤트
+    const tags = document.querySelector('.tags');
+    tags?.addEventListener('click', (e) => {
+        const target = e.target as HTMLElement;
+        const tag = target.closest('.tag') as HTMLElement;
 
-        // 태그 토글 가능
-        if (classList.contains('tag')) {
-            console.log(classList);
-            toggleTag(target);
+        if (tag) {
+            console.log(tag);
+            toggleTag(tag);
+            render();
         }
     });
 
 
-    // todo: 아이템 마다 이벤트 위임
-    // 할일 삭제
+    // 아이템 마다 이벤트 위임
+    const items = document.querySelector('.items');
+    // click 이벤트
+    items?.addEventListener('click', (e) => {
+        const target = e.target as HTMLElement;
+        const item = target.closest('.item');
+        const checkBtn = target.closest('.btn-check');
+        const removeBtn = target.closest('.btn-remove');
 
-    // 완료 토글
+        if (item) {
+            // 할일 완료
+            if (checkBtn) {
+                item.classList.toggle('completed');
+            }
+            // 할일 삭제
+            if (removeBtn) {
+                console.log(Number(item.id));
+                removeItem(Number(item.id));
+                render();
+            }
+        }
+    });
 
-    // drag&drop
+    // mousedown, mouseup, mousemove 이벤트
+    items?.addEventListener('mousedown', (e) => {
+        const target = e.target as HTMLElement;
+        const item = target.closest('.item');
+        const dragBtn = target.closest('.btn-drag');
+
+        if (item && dragBtn) {
+            console.log('mousedown', item);
+        }
+    });
+
+    items?.addEventListener('mouseup', (e) => {
+        const target = e.target as HTMLElement;
+        const item = target.closest('.item');
+        const dragBtn = target.closest('.btn-drag');
+
+        if (item && dragBtn) {
+            console.log('mouseup', item);
+        }
+    });
+
+    items?.addEventListener('mousemove', (e) => {
+        const target = e.target as HTMLElement;
+        const item = target.closest('.item');
+        const dragBtn = target.closest('.btn-drag');
+
+        if (item && dragBtn) {
+            console.log('mousemove', item);
+        }
+    });
 };
+
